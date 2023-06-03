7c62b52f34940c4130c917bd67611ad3bccbd32a
Rajesh Pradhan <pradhan143rajesh@gmail.com>
2023-06-03 13:39:38 +0530

    add again

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const cors = require("cors");
-
-// Enable CORS middleware
-server.use(cors());
 
 server.use(middlewares);
-server.use(router);
+server.use("", router);
 server.listen(process.env.PORT || 8000, () => {
   console.log("JSON Server is running");
 });
